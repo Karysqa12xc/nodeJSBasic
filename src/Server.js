0d70a989e7b398dd6a27aafdf68f8d3adc491150
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2023-06-28 17:14:14 +0700

    setups viewEngine and download library

---
+++ src/Server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+const app = express();
+const port = 3000
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render(`index.ejs`);
+})
+
+app.get('/about', (req, res)=>{
+    res.send(`My name is nam`);
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
