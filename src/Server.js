bb8aa229fb088156c11d88e83cf8c8aeb516f95e
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2023-07-15 09:03:13 +0700

    Middlware

--- src/Server.js
+++ src/Server.js
@@ -1,22 +1,34 @@
 import express from 'express';
 import 'dotenv/config';
 import configViewEngine from './configs/viewEngine';
+var morgan = require('morgan');
 const app = express();
+const port = process.env.PORT || 8080;
+
 import initWebRoute from "./route/web";
 import initApiRoute from "./route/api";
 app.use(express.urlencoded({extende: true}));
 app.use(express.json());
+
+app.use(morgan('combined'));
+app.use((req, res, next) =>{
+    console.log(">>>run into my middleware");
+    console.log(req.method);
+    next();
+})
 // import connection from './configs/connectDB';
-const port = process.env.PORT || 8080;
+
+
 //setup view engine
 configViewEngine(app);
 //init web route
 initWebRoute(app);
 //init api route
 initApiRoute(app);
-
-
-
+//handle 404 not found
+app.use((req, res) =>{
+  return res.render("404.ejs");
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 }) 
